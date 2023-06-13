e3f95305202b3b77167b7d69d7b9887d1050879f
Alex Rans <alexrans@protonmail.com>
2023-06-13 23:28:16 +0200

    commit for aws

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../../vinylshop/vendor/autoload.php';
+require __DIR__.'/../../htdocs/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../../vinylshop/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../../vinylshop/bootstrap/app.php';
+$app = require_once __DIR__.'/../../htdocs/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
